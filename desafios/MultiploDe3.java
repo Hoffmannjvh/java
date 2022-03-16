1e1dcb8574f800c6efbd51b7b5ece728aaea990e
João Vitor Hoffmann <contato.jhonnybjj@gmail.com>
2022-03-15 22:55:34 -0300

    att

---
+++ desafios/MultiploDe3.java
@@ -0,0 +1,10 @@
+package desafios;
+
+public class MultiploDe3 {
+    public static void main(String[] args) {
+        for (int multiplo = 1; multiplo < 100; multiplo+= 3) {
+            System.out.println(multiplo);
+
+        }
+    }
+}
